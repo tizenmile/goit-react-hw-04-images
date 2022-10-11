e4ab32a998bc7705c6413236160c0a13c960da1b
tizenmile <tizen48@gmail.com>
2022-10-11 09:38:06 +0300

    add react hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,62 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { SearchBar } from '../Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from "./Button/Button";
 import { Loader } from "./Loader/Loader";
 import axios from 'axios';
 
-export class App extends Component {
-  state = {
-    apiResponse: [],
-    search: null,
-    page: 1,
-    loading: false,
-    isError: false
-  }
+export const App = () => {
+  const [apiResponse, setApiResponse] = useState([])
+  const [search, setSearch] = useState(null)
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
-  fetchFromPixabay = async () => {
+
+  useEffect(() => {
     axios.get(`https://pixabay.com/api/`, {
       params: {
         key: '29059571-ec18f33066846ba93eec17670',
-        q: this.state.search,
+        q: search,
         image_type: `photo`,
         orientation: 'horizontal',
         per_page: 12,
-        page: this.state.page
+        page
       }
     })
       .then(async res => {
-        res.data.hits.length ? this.setState({ isError: true }) : this.setState({ isError: false })
-        await this.setState((prevState) => ({
-          apiResponse: [...prevState.apiResponse, ...res.data.hits]
-        }));
+        if (search === null || search === '') return
+        res.data.hits.length ? setIsError(true) : setIsError(false)
+        await setApiResponse((prevState) => ([...prevState, ...res.data.hits]));
       })
       .finally(() => {
-        this.setState({ loading: false });
+        setIsError(false)
+        setLoading(false)
       });
-  }
+  }, [search, page]);
 
 
-  searchQuery = async (event) => {
+  const searchQuery = async (event) => {
     event.preventDefault()
-    this.setState({ loading: true, apiResponse: '', page: 1 })
-    await this.setState({ search: event.target.search.value })
-    await this.fetchFromPixabay()
+    setLoading(true)
+    setApiResponse('')
+    setPage(1)
+    await setSearch(event.target.search.value)
   }
 
-  loadMore = async () => {
-    this.setState({ loading: true })
-    await this.setState(prev => ({ page: prev.page += 1 }))
-    await this.fetchFromPixabay()
+  const loadMore = async () => {
+    setLoading(true)
+    await setPage(prev => (prev += 1))
   }
-  render() {
-    return (
-      <div>
-        <SearchBar searchQuery={this.searchQuery} />
-        {this.state.apiResponse.length ? <ImageGallery result={this.state.apiResponse} /> : <></>}
-        {this.state.loading && <Loader />}
-        {this.state.apiResponse.length || this.state.isError ? <Button loadMore={this.loadMore} /> : <></>}
 
-      </div>
-    )
-  }
+  return (
+    <div>
+      <SearchBar searchQuery={searchQuery} />
+      {apiResponse.length ? <ImageGallery result={apiResponse} /> : <></>}
+      {loading && <Loader />}
+      {apiResponse.length || isError ? <Button loadMore={loadMore} /> : <></>}
+
+    </div>
+  )
+
 }
